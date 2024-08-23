be33b427e711fe147fd537eb499312cad67b713a
TheMostLocal <123339962+TheMostLocal@users.noreply.github.com>
2024-08-23 10:00:12 +0000

    new classes

---
+++ GlobalStore.Domain/Catalog/CatalogController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using GlobalStore.Domain.Catalog;
+
+namespace GlobalStore.Api.Controllers
+{
+    [ApiCoontroller]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+    
+    }
+    [HttpGet]
+    public IActionResult GetItems()
+    {
+        var items = new List<Item>()
+        {
+            new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+            new Item("Shorts", "Ohio State shorts.", "Nike", 49.99m)
+        };
+        return Ok(items);
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # GlobalStore-api
 Starting at Lab 4 
 https://osu.instructure.com/courses/141112/files/folder/Labs?preview=52857569
+
+Stopped at Lab 4, Step 3 - Catalog API - Get Item by Id , Page 11
+
+Need to ensure all of Step 2 is correct 
+dotnet restore && dotnet build
