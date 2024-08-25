b8cda92341b7c205652fde5b05f6989f4a502129
TheMostLocal <123339962+TheMostLocal@users.noreply.github.com>
2024-08-25 18:16:10 +0000

    Lab 4, step 4 and 5 completed

--- GlobalStore.Domain/Catalog/CatalogController.cs
+++ GlobalStore.Domain/Catalog/CatalogController.cs
@@ -18,5 +18,18 @@ namespace GlobalStore.Api.Controllers
         };
         return Ok(items);
     }
+    [HttpGet("{id: int}" )]
+    public IActionResult GetItem(int id)
+    {
+    var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+    item.Id = id;
+
+    return Ok(item);
+    }
+    [Http Post]
+    public IActionResult Post(Item item)
+    {
+        return Created("/catalog/42", item);
+    }
     }
 }
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Starting at Lab 4 
 https://osu.instructure.com/courses/141112/files/folder/Labs?preview=52857569
 
-Stopped at Lab 4, Step 3 - Catalog API - Get Item by Id , Page 11
+Stopped at Lab 4, Step 5 - Catalog API - Post New Rating by Item Id , Page 14
 
-Need to ensure all of Step 2 is correct 
+Need to ensure all of Steps are correct
 dotnet restore && dotnet build
