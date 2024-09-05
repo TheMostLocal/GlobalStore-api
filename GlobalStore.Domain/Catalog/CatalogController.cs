4ebe260cd01d164b5dc202f21f661fd41bc456df
TheMostLocal <123339962+TheMostLocal@users.noreply.github.com>
2024-09-05 21:07:35 +0000

    End Part 1, Lab 4

--- GlobalStore.Domain/Catalog/CatalogController.cs
+++ GlobalStore.Domain/Catalog/CatalogController.cs
@@ -4,7 +4,7 @@ using GlobalStore.Domain.Catalog;
 namespace GlobalStore.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/controller")]
     public class CatalogController : ControllerBase
     {
     
@@ -32,4 +32,24 @@ namespace GlobalStore.Api.Controllers
         return Created("/catalog/42", item);
     }
     }
+}
+[HttpPost("(id: int}/ratings" )]
+public IActionResult PostRating(int id, [FromBody] Rating rating)
+{
+    var item = new Item "Shirt", "Ohio State shirt.", "Nike", 29.99m);
+    item.Id = id;
+    item.AddRating(rating);
+    
+    return Ok(item);
+}
+[HttpPut("{id:int}")]
+public IActionResult Put(int id, Item item)
+{
+    return NoContent();
+}
+
+[HttpPut("{id:int}")]
+public IActionResult Delete(int id)
+{
+    return NoContent();
 }
